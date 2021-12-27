85e5ff3ac1c20f00fda6f156574b32b10a647249
Esper0328 <yoheikato@katouyouheinoMacBook-Air.local>
2021-12-27 11:36:11 +0900

    modified display and output logic

--- posting.py
+++ posting.py
@@ -48,9 +48,9 @@ def displayAccount(transaction_list, account_list):
         creditor_list = []
         for transaction in transaction_list:
             if(account == transaction.debtor):
-                debtor_list.append(transaction.date + "'" + transaction.debtor + "'" + transaction.debtor_charge)
+                debtor_list.append(transaction.date + "," + transaction.debtor + "," + transaction.debtor_charge)
             if(account == transaction.creditor):
-                creditor_list.append(",,," + transaction.date + "'" + transaction.creditor + "'" + transaction.creditor_charge)
+                creditor_list.append(transaction.date + "," + transaction.creditor + "," + transaction.creditor_charge)
         numberOfDebtorItem = len(debtor_list)
         numberOfCreditorItem = len(creditor_list)
         if(numberOfDebtorItem < numberOfCreditorItem):
@@ -63,18 +63,55 @@ def displayAccount(transaction_list, account_list):
             print("Nothing")
         elif((numberOfDebtorItem != 0) and (numberOfCreditorItem == 0)):
             for index in range(numberOfLarger):
-                print(transaction.date + "'" + transaction.debtor + "'" + transaction.debtor_charge)
+                print(debtor_list[index])
         elif((numberOfDebtorItem == 0) and (numberOfCreditorItem != 0)):
             for index in range(numberOfLarger):
-                print(",,," + transaction.date + "'" + transaction.creditor + "'" + transaction.creditor_charge)
+                print(",,," + creditor_list[index])
         else:
             for index in range(numberOfLarger):
                 if((index < numberOfDebtorItem) and (index < numberOfCreditorItem)):
-                    print(transaction.date + "'" + transaction.debtor + "'" + transaction.debtor_charge + "'" + transaction.date + "'" + transaction.creditor + "'" + transaction.creditor_charge)
+                    print(debtor_list[index] + "," + creditor_list[index])
                 elif((index < numberOfDebtorItem) and (index >= numberOfCreditorItem)):
-                    print(transaction.date + "'" + transaction.debtor + "'" + transaction.debtor_charge)
+                    print(debtor_list[index])
                 elif((index >= numberOfDebtorItem) and (index < numberOfCreditorItem)):
-                    print(transaction.date + "'" + transaction.creditor + "'" + transaction.creditor_charge)
+                    print(",,," + creditor_list[index])
+        print("")
+
+def writeAccount(transaction_list, account_list, outputfile):
+    for account in account_list:
+        outputfile.write(account + "\n")
+        debtor_list = []
+        creditor_list = []
+        for transaction in transaction_list:
+            if(account == transaction.debtor):
+                debtor_list.append(transaction.date + "," + transaction.debtor + "," + transaction.debtor_charge)
+            if(account == transaction.creditor):
+                creditor_list.append(transaction.date + "," + transaction.creditor + "," + transaction.creditor_charge)
+        numberOfDebtorItem = len(debtor_list)
+        numberOfCreditorItem = len(creditor_list)
+        if(numberOfDebtorItem < numberOfCreditorItem):
+            numberOfLarger = numberOfCreditorItem
+            numberOfSmaller = numberOfDebtorItem 
+        else:
+            numberOfLarger = numberOfDebtorItem
+            numberOfSmaller = numberOfCreditorItem
+        if((numberOfDebtorItem == 0) and (numberOfCreditorItem == 0)):
+            outputfile.write("Nothing" + "\n")
+        elif((numberOfDebtorItem != 0) and (numberOfCreditorItem == 0)):
+            for index in range(numberOfLarger):
+                outputfile.write(debtor_list[index] + "\n")
+        elif((numberOfDebtorItem == 0) and (numberOfCreditorItem != 0)):
+            for index in range(numberOfLarger):
+                outputfile.write(",,," + creditor_list[index] + "\n")
+        else:
+            for index in range(numberOfLarger):
+                if((index < numberOfDebtorItem) and (index < numberOfCreditorItem)):
+                    outputfile.write(debtor_list[index] + "," + creditor_list[index] + "\n")
+                elif((index < numberOfDebtorItem) and (index >= numberOfCreditorItem)):
+                    outputfile.write(debtor_list[index] + "\n")
+                elif((index >= numberOfDebtorItem) and (index < numberOfCreditorItem)):
+                    outputfile.write(",,," + creditor_list[index] + "\n")
+        outputfile.write("\n")
 
 transaction_list = []
 account_list = []
@@ -88,7 +125,13 @@ if len(args) == 2:
             if(len(items[0]) != 0):
                 transaction = Transaction(items[0], items[1], items[2], items[3], items[4])
                 transaction_list.append(transaction)
+#    for transaction in transaction_list:
+#        print(transaction.date)
     account_list = findAccount(transaction_list)
+#    for account in account_list:
+#        print(account)
     displayAccount(transaction_list, account_list)
+    with open('output.csv', 'w') as outputfile:
+        writeAccount(transaction_list, account_list, outputfile)
 else:
     print('Invalid Number of Argument')
